d111d49d4625aec3867744a547f23c2b11150b2f
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-22 00:25:42 +0900

    Create Authenticate

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/message/Authenticate.scala
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.message
+
+import java.util.Arrays.copyOf
+
+import cats.syntax.all.*
+
+import scodec.*
+import scodec.bits.*
+import scodec.interop.cats.*
+
+case class Authenticate(user: String, hashedPassword: Array[Byte], pluginName: String) extends Message:
+
+  override def payloadSize: Int = 4 + 4 + 1 + 23 + user.length + 1 + hashedPassword.length + pluginName.length + 2
+
+  override protected def encodeBody: Attempt[BitVector] = Authenticate.encoder.encode(this)
+
+  override def encode: BitVector =
+    encodeBody.require
+
+  override def toString: String = "Authenticate"
+
+object Authenticate:
+
+  val encoder: Encoder[Authenticate] = Encoder {
+    auth =>
+      val capabilityFlags = hex"07a23e19".bits
+      val maxPacketSize = hex"ffffff00".bits
+      val characterSet = hex"ff".bits
+      val userBytes = auth.user.getBytes("UTF-8")
+
+      val reserved = BitVector.fill(23 * 8)(false) // 23 bytes of zero
+
+      val pluginBytes = auth.pluginName.getBytes("UTF-8")
+
+      Attempt.successful(
+        capabilityFlags |+|
+          maxPacketSize |+|
+          characterSet |+|
+          reserved |+|
+          BitVector(copyOf(userBytes, userBytes.length + 1)) |+|
+          BitVector(copyOf(auth.hashedPassword, auth.hashedPassword.length)) |+|
+          BitVector(copyOf(pluginBytes, pluginBytes.length + 2))
+      )
+  }
