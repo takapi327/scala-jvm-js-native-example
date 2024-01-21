64da0f35a632b663acdd3644044d3f9dec2b77f1
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-21 22:00:44 +0900

    Add Dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,4 +5,11 @@ object Dependencies {
 
   val cats       = "org.typelevel" %% "cats-core"   % "2.10.0"
   val catsEffect = "org.typelevel" %% "cats-effect" % "3.5.2"
+
+  val fs2 = Seq(
+    "fs2-core",
+    "fs2-io"
+  ).map("co.fs2" %% _ % "3.9.3")
+
+  val ip4s = "com.comcast" %% "ip4s-core" % "3.2.0"
 }
