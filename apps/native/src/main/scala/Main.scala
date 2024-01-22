64037e062f43fa24f6588a3ca03d1861d9c5754b
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-23 00:45:01 +0900

    Create Main

---
+++ apps/native/src/main/scala/Main.scala
@@ -0,0 +1,32 @@
+
+import scala.concurrent.duration.*
+
+import cats.effect.*
+//import epollcat.EpollApp
+
+import fs2.*
+import fs2.io.net.*
+
+import org.typelevel.otel4s.trace.Tracer
+
+import ldbc.connector.*
+
+object Main extends IOApp:
+
+  given Tracer[IO] = Tracer.noop[IO]
+
+  val session: Resource[IO, Session[IO]] =
+    Session.single(
+      host = "127.0.0.1",
+      port = 13306,
+      user = "root",
+      password = Some("root"),
+      debug = true,
+      ssl = SSL.None,
+    )
+
+  override def run(args: List[String]): IO[ExitCode] =
+    session.use { session =>
+      IO.sleep(5.seconds) *>
+        IO.pure(ExitCode.Success)
+    }
