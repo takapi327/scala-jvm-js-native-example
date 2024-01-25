294937c4db4690f3e71e21a8aee2fea504b7177e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-25 19:51:05 +0900

    Create AuthenticationPacket

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/AuthenticationPacket.scala
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+
+import scala.annotation.switch
+
+import scodec.*
+import scodec.codecs.*
+
+trait AuthenticationPacket extends Packet
+
+object AuthenticationPacket:
+  
+  val decoder: Decoder[AuthenticationPacket] =
+    int8.flatMap { status =>
+      (status: @switch) match
+        case AuthMoreDataPacket.STATUS => AuthMoreDataPacket.decoder
+        case OKPacket.STATUS           => OKPacket.decoder
+    }
