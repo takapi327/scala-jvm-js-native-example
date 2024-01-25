e688970e804f6daf47d7a85e4580f640caa137ac
takapi327 <t.takapi0327@gmail.com>
2024-01-25 16:12:40 +0900

    Create ComQuery message

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/message/ComQuery.scala
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.message
+
+import java.util.Arrays.copyOf
+
+import cats.syntax.all.*
+
+import scodec.*
+import scodec.bits.*
+import scodec.interop.cats.*
+
+case class ComQuery(sql: String) extends Message:
+
+  override protected def encodeBody: Attempt[BitVector] =
+    ComQuery.encoder.encode(this)
+
+  override def encode: BitVector = encodeBody.require
+
+  override def toString: String = "COM_QUERY Request"
+
+object ComQuery:
+
+  val encoder: Encoder[ComQuery] = Encoder { comQuery =>
+
+    val sqlBytes = comQuery.sql.getBytes("UTF-8")
+
+    Attempt.successful(
+      BitVector(0x03) |+|
+        BitVector(0x00) |+|
+        BitVector(0x01) |+|
+        BitVector(copyOf(sqlBytes, sqlBytes.length))
+    )
+  }
