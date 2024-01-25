fa7034142b1ed4f2b16e43c83da8e2f893019ecd
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-25 19:51:25 +0900

    Create OKPacket

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/OKPacket.scala
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+
+import scodec.*
+import scodec.codecs.*
+
+/**
+ * An OK packet is sent from the server to the client to signal successful completion of a command.
+ * 
+ * As of MySQL 5.7.5, OK packets are also used to indicate EOF, and EOF packets are deprecated.
+ * 
+ * if CLIENT_PROTOCOL_41 is set, the packet contains a warning count.
+ *
+ * @param status
+ *   Type: int<1>
+ *   Name: header
+ *   Description: 0x00 or 0xFE the OK packet header
+ * @param affectedRows
+ *   Type: int<lenenc>
+ *   Name: affected_rows
+ *   Description: affected rows
+ * @param lastInsertId
+ *   Type: int<lenenc>
+ *   Name: last_insert_id
+ *   Description: last insert-id
+ * @param statusFlags if capabilities & CLIENT_PROTOCOL_41
+ *   Type: int<2>
+ *   Name: status_flags
+ *   Description: SERVER_STATUS_flags_enum
+ * @param warnings if capabilities & CLIENT_PROTOCOL_41
+ *   Type: int<2>
+ *   Name: warnings
+ *   Description: number of warnings
+ * @param info
+ *   Type: string<lenenc>
+ *   Name: info
+ *   Description: human readable status information
+ * @param sessionStateInfo
+ *   Type: string<lenenc>
+ *   Name: session state info
+ *   Description: Session State Information
+ */
+case class OKPacket(
+  status: Int,
+  affectedRows: Int,
+  lastInsertId: Int,
+  statusFlags: Int,
+  warnings: Option[Int],
+  info: Int,
+  sessionStateInfo: Option[Int]
+) extends AuthenticationPacket:
+
+  override def toString: String = "OK_Packet"
+
+object OKPacket:
+
+  val STATUS = 0
+
+  val decoder: Decoder[OKPacket] =
+    for
+      status <- uint4
+      affectedRows <- uint4
+      lastInsertId <- uint4
+      statusFlags <- uint4
+      warnings <- uint4
+      info <- uint4
+      sessionStateInfo <- uint4
+    yield OKPacket(status, affectedRows, lastInsertId, statusFlags, Some(warnings), info, Some(sessionStateInfo))
