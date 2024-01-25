cc2f586bdd2b35a425aea6b40cececa71b29fc1e
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-25 23:20:29 +0900

    Create ColumnsNumberPacket

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/ColumnsNumberPacket.scala
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+
+import scodec.*
+import scodec.codecs.*
+
+case class ColumnsNumberPacket(
+  columnCount: Int
+) extends Packet:
+
+  override def toString: String = "ColumnsNumber Packet"
+
+object ColumnsNumberPacket:
+  
+  val decoder: Decoder[ColumnsNumberPacket] = uint8.map(ColumnsNumberPacket(_))
