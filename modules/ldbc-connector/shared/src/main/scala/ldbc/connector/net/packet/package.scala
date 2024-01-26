64a90b523294c7d38781642180c321df679979ec
takapi327 <t.takapi0327@gmail.com>
2024-01-26 21:05:02 +0900

    Add java.time decoder

--- modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/package.scala
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/package.scala
@@ -7,6 +7,7 @@
 package ldbc.connector.net
 
 import scodec.*
+import scodec.codecs.*
 import scodec.bits.BitVector
 
 package object packet:
@@ -22,3 +23,26 @@ package object packet:
       val string = new String(bytes.toArray, java.nio.charset.StandardCharsets.UTF_8)
       val remainder = bits.drop((bytes.size + 1) * 8) // +1 is a null character, so *8 is a byte to bit
       Attempt.successful(DecodeResult(string, remainder))
+
+  def time: Decoder[java.time.LocalTime] =
+    for
+      hour <- uint8
+      minute <- uint8
+      second <- uint8
+      microsecond <- uint32L
+    yield
+      java.time.LocalTime.of(hour, minute, second, microsecond.toInt * 1000)
+
+  def timestamp(length: Int): Decoder[java.time.LocalDateTime] =
+    bytes(length).asDecoder.map(_.decodeUtf8 match
+      case Left(value) => throw value
+      case Right(value) => java.time.LocalDateTime.parse(value, java.time.format.DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+    )
+
+  def date: Decoder[java.time.LocalDate] =
+    for
+      year <- uint16L
+      month <- uint8
+      day <- uint8
+    yield
+      java.time.LocalDate.of(year, month, day)
