a297984a035f7ca8aea93b3b2dce228e3e7b25dc
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-28 00:55:44 +0900

    Create Type

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/data/Type.scala
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.data
+
+import cats.Eq
+import cats.syntax.all._
+
+final case class Type(name: String, componentTypes: List[Type] = Nil)
+
+object Type:
+
+  given EqType: Eq[Type] = Eq.fromUniversalEquals
+
+  //@deprecated("Use Type.int instead", "0.1.0")
+  def tinyint(n: Int): Type = Type(s"tinyint($n)")
+  val tinyint: Type = Type("tinyint")
+
+  //@deprecated("Use Type.bigint instead", "0.1.0")
+  def bigint(n: Int): Type = Type(s"bigint($n)")
+  val bigint: Type = Type("bigint")
+
+  def varchar(n: Int): Type = Type(s"varchar($n)")
+  val varchar: Type = Type("varchar")
+
+  def time(n: Int): Type = Type(s"time($n)")
+  val time: Type = Type("time")
+  
+  def timestamp(n: Int): Type = Type(s"timestamp($n)")
+  val timestamp: Type = Type("timestamp")
