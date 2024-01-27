692bbe1e0c579441785a5c5675203888bfb9a7bf
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-28 00:57:10 +0900

    Create NumericCodecs

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/codec/NumericCodecs.scala
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.codec
+
+import ldbc.connector.Codec
+import ldbc.connector.data.Type
+
+trait NumericCodecs:
+
+  private def safe[A](f: String => A): String => Either[String, A] = s =>
+    try Right(f(s))
+    catch {
+      case _: NumberFormatException => Left(s"Invalid: $s")
+    }
+
+  val tinyint: Codec[Short] = Codec.simple(_.toString, safe(_.toShort), Type.tinyint)
+  def tinyint(n: Int): Codec[Short] = Codec.simple(_.toString, safe(_.toShort), Type.tinyint(n))
+  
+  val bigint: Codec[Long] = Codec.simple(_.toString, safe(_.toLong), Type.bigint)
+  def bigint(n: Int): Codec[Long] = Codec.simple(_.toString, safe(_.toLong), Type.bigint(n))
