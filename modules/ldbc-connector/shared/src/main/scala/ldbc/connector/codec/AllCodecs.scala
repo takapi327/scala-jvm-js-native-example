c7605d01e6e0be9ecca6235c7b9c31af7865d7b0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-28 00:57:30 +0900

    Create AllCodecs

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/codec/AllCodecs.scala
@@ -0,0 +1,11 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.codec
+
+trait AllCodecs extends NumericCodecs, TextCodecs, TemporalCodecs
+
+object all extends AllCodecs
