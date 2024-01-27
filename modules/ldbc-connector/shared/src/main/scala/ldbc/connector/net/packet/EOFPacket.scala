b932112b02382b8140cee914de64909d7529da08
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-27 19:24:54 +0900

    Create EOFPacket

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/EOFPacket.scala
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+
+import scodec.*
+import scodec.codecs.*
+
+case class EOFPacket(
+  status: Int,
+  warnings:         Int,
+  statusFlags:      Int,
+) extends GenericResponsePackets:
+
+  override def toString: String = s"EOF_Packet"
+  
+object EOFPacket:
+
+  val STATUS = 0xFE
+  
+  val decoder: Decoder[EOFPacket] =
+    for
+      status <- uint4
+      warnings <- uint4
+      statusFlags <- uint4
+    yield
+      EOFPacket(status, warnings, statusFlags)
