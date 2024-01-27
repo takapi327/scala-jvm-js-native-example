7a8a2f432e27b56f6ca12f10937f4bb8a20558c6
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-28 00:57:20 +0900

    Create TextCodecs

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/codec/TextCodecs.scala
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.codec
+
+import cats.syntax.all.*
+
+import ldbc.connector.Codec
+import ldbc.connector.data.Type
+
+trait TextCodecs:
+  
+  val varchar: Codec[String] = Codec.simple(s => s, _.asRight, Type.varchar)
+  def varchar(size: Int): Codec[String] = Codec.simple(s => s, _.asRight, Type.varchar(size))
