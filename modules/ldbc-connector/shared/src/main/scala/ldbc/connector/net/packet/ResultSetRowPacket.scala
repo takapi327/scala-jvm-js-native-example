10deab8fca64727eae4df626aedf92a36cc07640
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-27 19:26:18 +0900

    Fix ResultSetRowPacket

--- modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/ResultSetRowPacket.scala
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/ResultSetRowPacket.scala
@@ -12,19 +12,30 @@ import scodec.interop.cats.*
 
 import cats.syntax.all.*
 
-import ldbc.connector.util.DataType
+import ldbc.connector.data.*
 
-case class ResultSetRowPacket(value: Seq[String | java.time.LocalTime | java.time.LocalDateTime | java.time.LocalDate | None.type]) extends Packet:
+case class ResultSetRowPacket(value: Seq[Column[Byte | Int | Short | Long | String | java.time.LocalTime | java.time.LocalDate | java.time.LocalDateTime]]) extends Packet:
   override def toString: String = s"ProtocolText::ResultSetRow"
 
 object ResultSetRowPacket:
+  
+  private def columnDecode(column: ColumnDefinitionPacket, length: Int): Decoder[Column[Byte | Int | Short | Long | String | java.time.LocalTime | java.time.LocalDate | java.time.LocalDateTime]] =
+    column.columnType match
+      case ColumnDataType.MYSQL_TYPE_TIMESTAMP => timestamp(length).asDecoder.map(value => Column[java.time.LocalDateTime](column, value))
+      case ColumnDataType.MYSQL_TYPE_DATE => date.asDecoder.map(value => Column[java.time.LocalDate](column, value))
+      case ColumnDataType.MYSQL_TYPE_TIME => time.asDecoder.map(value => Column[java.time.LocalTime](column, value))
+      case ColumnDataType.MYSQL_TYPE_TINY if column.flags.contains(ColumnDefinitionFlags.UNSIGNED_FLAG) => uint8.asDecoder.map(value => Column[Short](column, value.toShort))
+      case ColumnDataType.MYSQL_TYPE_TINY => bytes(length).map(_.decodeUtf8Lenient.toShort).asDecoder.map(value => Column[Short](column, value))
+      case ColumnDataType.MYSQL_TYPE_LONGLONG if column.flags.contains(ColumnDefinitionFlags.UNSIGNED_FLAG) =>
+        bytes(length).map(_.decodeUtf8Lenient.toLong).asDecoder.map(value => Column[Long](column, value))
+      case ColumnDataType.MYSQL_TYPE_LONGLONG => int64L.asDecoder.map(value => Column[Long](column, value))
+      case _ => bytes(length).asDecoder.map(_.decodeUtf8Lenient).map(value => Column[String](column, value))
 
-  def decoder(columns: Seq[ColumnDefinitionPacket]): Decoder[ResultSetRowPacket] =
-    columns.traverse(column => uint8.flatMap(length =>
-      if length == 0xfe then Decoder.pure(None)
-      else column.columnType match
-        case DataType.MYSQL_TYPE_TIMESTAMP => timestamp(length).asDecoder
-        case DataType.MYSQL_TYPE_DATE => date.asDecoder
-        case DataType.MYSQL_TYPE_TIME => time.asDecoder
-        case _ => bytes(length).asDecoder.map(_.decodeUtf8.getOrElse(""))
-    )).map(ResultSetRowPacket(_))
+  def decoder(columns: Seq[ColumnDefinitionPacket]): Decoder[ResultSetRowPacket | EOFPacket] =
+    uint8.flatMap {
+      case EOFPacket.STATUS => EOFPacket.decoder
+      case length => columns.zipWithIndex.traverse((column, index) =>
+        if index == 0 then columnDecode(column, length)
+        else uint8.flatMap(length => columnDecode(column, length))
+      ).map(ResultSetRowPacket(_))
+    }
