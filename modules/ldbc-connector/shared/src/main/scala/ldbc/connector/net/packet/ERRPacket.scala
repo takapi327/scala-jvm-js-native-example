bb221ed337647b24c1ba0c533c248dbb3773d9db
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-27 19:25:09 +0900

    Create ERRPacket

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/ERRPacket.scala
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+
+import scodec.*
+import scodec.codecs.*
+
+case class ERRPacket(
+  status: Int,
+  errorCode: Int,
+  sqlStateMarker: String,
+  sqlState: String,
+  errorMessage: String
+) extends GenericResponsePackets
+
+object ERRPacket:
+  
+  val STATUS = 0xFF
+  
+  val decoder: Decoder[ERRPacket] =
+    for
+      status <- uint4
+      errorCode <- uint2
+      sqlStateMarker <- bytes(1)
+      sqlState <- bytes(5)
+      errorMessage <- variableSizeBytes(uint8, utf8)
+    yield ERRPacket(status, errorCode, sqlStateMarker.decodeUtf8.toOption.get, sqlState.decodeUtf8.toOption.get, errorMessage)
