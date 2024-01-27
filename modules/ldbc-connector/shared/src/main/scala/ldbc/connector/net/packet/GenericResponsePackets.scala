6e48bcc3890c07faf37030b75eeef189b5d3d130
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-27 19:24:41 +0900

    Create GenericResponsePackets

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/packet/GenericResponsePackets.scala
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.packet
+
+import scala.annotation.switch
+
+import scodec.*
+import scodec.codecs.*
+
+trait GenericResponsePackets extends Packet
+
+object GenericResponsePackets:
+  
+  val decoder: Decoder[GenericResponsePackets] =
+    int8.flatMap { status =>
+      (status: @switch) match
+        case OKPacket.STATUS => OKPacket.decoder
+        case EOFPacket.STATUS => EOFPacket.decoder
+        case ERRPacket.STATUS => ERRPacket.decoder
+    }
