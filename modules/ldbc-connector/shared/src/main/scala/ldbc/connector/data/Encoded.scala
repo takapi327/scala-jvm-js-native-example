99a8b720909b3c920ac0df0186e77b0687f0d345
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-28 00:56:04 +0900

    Create Encoded

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/data/Encoded.scala
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.data
+
+final case class Encoded(value: String, redacted: Boolean):
+
+  override def toString: String = if (redacted) Encoded.RedactedText else value
+
+object Encoded:
+
+  def apply(value: String): Encoded = Encoded(value, false)
+
+  final val RedactedText: String = "?"
