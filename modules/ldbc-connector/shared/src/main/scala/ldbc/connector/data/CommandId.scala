8ea7a061e52e4cd2b91b8310ad5228f8d2904637
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-28 22:37:48 +0900

    Create CommandId

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/data/CommandId.scala
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.data
+
+object CommandId:
+  
+  val COM_QUERY = 0x03
+  val COM_STMT_PREPARE = 0x16
+  val COM_STMT_EXECUTE = 0x17
