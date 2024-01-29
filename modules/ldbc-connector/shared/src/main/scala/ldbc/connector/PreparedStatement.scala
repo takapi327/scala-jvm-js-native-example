9581eb41bad86e43b44fa74bacb32d311b5c5306
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-30 00:15:06 +0900

    Fix PreparedStatement

--- modules/ldbc-connector/shared/src/main/scala/ldbc/connector/PreparedStatement.scala
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/PreparedStatement.scala
@@ -6,14 +6,17 @@
 
 package ldbc.connector
 
-import cats.Monad
+//import cats.Monad
 import cats.data.StateT
 import cats.syntax.all.*
 
-import ldbc.connector.net.message.ComStmtExecute
+import cats.effect.*
+
+import ldbc.connector.net.message.*
+import ldbc.connector.net.packet.*
 import ldbc.connector.util.DataType
 
-class PreparedStatement[F[_]: Monad](statementId: Long, numParams: Int, bms: BufferedMessageSocket[F]):
+class PreparedStatement[F[_]: Concurrent](statementId: Long, numParams: Int, bms: BufferedMessageSocket[F]):
 
   def setInt(index: Int, value: Int): PreparedStatement.QueryState[F, Unit] =
     StateT.modify[F, Map[Int, String]](_ + (index -> value.toString))
@@ -21,15 +24,48 @@ class PreparedStatement[F[_]: Monad](statementId: Long, numParams: Int, bms: Buf
   def setString(index: Int, value: String): PreparedStatement.QueryState[F, Unit] =
     StateT.modify[F, Map[Int, String]](_ + (index -> s"'$value'"))
 
-  def executeQuery(): F[Unit] =
-    bms.changeCommandPhase *>
-      bms.send(
+  private def repeatProcess[P <: Packet](times: Int, decoder: scodec.Decoder[P]): F[List[P]] =
+    def read(remaining: Int, acc: List[P]): F[List[P]] =
+      if remaining <= 0 then Concurrent[F].pure(acc)
+      else bms.receive(decoder).flatMap(result => read(remaining - 1, acc :+ result))
+
+    read(times, List.empty[P])
+
+  private def readUntilEOF(
+    columns: List[ColumnDefinitionPacket],
+    acc: List[BinaryProtocolResultSetRowPacket]
+  ): F[List[BinaryProtocolResultSetRowPacket]] =
+    bms.receive(BinaryProtocolResultSetRowPacket.decoder(columns)).flatMap {
+      case _: EOFPacket => Concurrent[F].pure(acc)
+      case _: ERRPacket => Concurrent[F].raiseError(new RuntimeException("Error packet received"))
+      case row: BinaryProtocolResultSetRowPacket => readUntilEOF(columns, acc :+ row)
+    }
+
+  def executeQuery[A](codec: ldbc.connector.Codec[A]): F[List[A]] =
+    for
+      columnCount <- bms.changeCommandPhase *> bms.send(
         ComStmtExecute(
           statementId,
           numParams,
-          Map(DataType.MYSQL_TYPE_LONGLONG -> 1L, DataType.MYSQL_TYPE_VAR_STRING -> "foo")
+          Map(DataType.MYSQL_TYPE_LONGLONG -> 1L, DataType.MYSQL_TYPE_VAR_STRING -> "Category 1")
         )
-      )
+      ) *> bms.receive(ColumnsNumberPacket.decoder)
+      columns      <- repeatProcess(columnCount.columnCount, ColumnDefinitionPacket.decoder)
+      resultSetRow <- readUntilEOF(columns, Nil)
+    yield resultSetRow.map(row =>
+      codec.decode(0, row.value) match
+        case Left(value) =>
+          val column = columns(value.offset)
+          throw new IllegalArgumentException(s"""
+                                                |==========================
+                                                |Failed to decode column: `${ column.name }`
+                                                |Decode To: ${ column.columnType } -> ${ value.`type`.name.toUpperCase }
+                                                |
+                                                |Message [ ${ value.message } ]
+                                                |==========================
+                                                |""".stripMargin)
+        case Right(value) => value
+    )
 
 object PreparedStatement:
 
