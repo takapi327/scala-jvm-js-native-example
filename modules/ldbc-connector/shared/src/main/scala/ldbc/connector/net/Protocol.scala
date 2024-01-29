acfab09da8bb133404296602e84b82feca80c9a0
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-30 00:16:17 +0900

    Fix Protocol

--- modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/Protocol.scala
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/Protocol.scala
@@ -117,12 +117,13 @@ object Protocol:
             )
 
         override def preparedStatement(sql: String): F[PreparedStatement[F]] =
-          for result <- bms.changeCommandPhase *> bms.send(ComStmtPrepare(sql)) *>
+          for
+            result <- bms.changeCommandPhase *> bms.send(ComStmtPrepare(sql)) *>
                           bms.receive(ComStmtPrepareOkPacket.decoder).flatMap {
                             case _: ERRPacket => Concurrent[F].raiseError(new Exception("Failed to prepare statement"))
                             case result: ComStmtPrepareOkPacket => Concurrent[F].pure(result)
                           }
-          yield
-            println(result)
-            PreparedStatement(result.statementId, result.numParams, bms)
+            _ <- repeatProcess(result.numParams, ParameterDefinitionPacket.decoder)
+            _ <- repeatProcess(result.numColumns, ColumnDefinitionPacket.decoder)
+          yield PreparedStatement(result.statementId, result.numParams, bms)
     }
