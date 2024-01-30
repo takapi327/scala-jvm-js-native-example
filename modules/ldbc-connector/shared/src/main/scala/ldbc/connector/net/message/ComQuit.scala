ce8a7a3a4caa388ad3d5fc4cf12925e4ad03494d
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-31 00:55:43 +0900

    Create ComQuit

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/message/ComQuit.scala
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.message
+
+import scodec.*
+import scodec.bits.BitVector
+
+import ldbc.connector.data.CommandId
+
+case class ComQuit() extends Message:
+
+  override protected def encodeBody: Attempt[BitVector] =
+    ComQuit.encoder.encode(this)
+
+  override def encode: BitVector = encodeBody.require
+
+  override def toString: String = "COM_QUIT Request"
+  
+object ComQuit:
+  
+  val encoder: Encoder[ComQuit] = Encoder(_ => Attempt.successful(BitVector(CommandId.COM_QUIT)))
