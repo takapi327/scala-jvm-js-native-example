79c17601679805247950e0a13665031f823dd758
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-01-31 23:52:40 +0900

    Create BinaryProtocolValue

---
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/message/BinaryProtocolValue.scala
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2023-2024 by Takahiko Tominaga
+ * This software is licensed under the MIT License (MIT).
+ * For more information see LICENSE or https://opensource.org/licenses/MIT
+ */
+
+package ldbc.connector.net.message
+
+import java.util.Arrays.copyOf
+
+import cats.syntax.all.*
+
+import scodec.*
+import scodec.bits.*
+import scodec.codecs.*
+
+import ldbc.connector.data.*
+
+case class BinaryProtocolValue(values: Map[ColumnDataType, Any]) extends Message:
+
+  override protected def encodeBody: Attempt[BitVector] =
+    BinaryProtocolValue.encoder.encode(this)
+
+  override def encode: BitVector = encodeBody.require
+
+object BinaryProtocolValue:
+
+  val encoder: Encoder[BinaryProtocolValue] = Encoder { binaryProtocolValue =>
+    Attempt.successful(binaryProtocolValue.values.foldLeft(BitVector.empty) {
+      case (acc, tuple) => tuple match
+        case (ColumnDataType.MYSQL_TYPE_TINY, value: Byte) => acc ++ uint8L.encode(value).require
+        case (ColumnDataType.MYSQL_TYPE_VARCHAR, value: String) =>
+          val bytes = value.getBytes("UTF-8")
+          acc ++ BitVector(copyOf(bytes, bytes.length))
+        case (_, unknown) => throw new IllegalArgumentException(s"Unsupported data type: $unknown")
+    })
+  }
