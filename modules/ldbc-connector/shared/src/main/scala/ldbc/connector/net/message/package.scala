fb0a6a4584c702e35fe1cab3b41a3fbe7da99268
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2024-02-05 00:07:21 +0900

    Fix nullBitmap

--- modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/message/package.scala
+++ modules/ldbc-connector/shared/src/main/scala/ldbc/connector/net/message/package.scala
@@ -6,11 +6,8 @@
 
 package ldbc.connector.net
 
-//import cats.syntax.all.*
-
 import scodec.bits.BitVector
 import scodec.codecs.*
-//import scodec.interop.cats.*
 
 import ldbc.connector.data.ColumnDataType
 
@@ -18,21 +15,12 @@ package object message:
 
   def nullBitmap(columns: List[ColumnDataType]): BitVector =
     if columns.nonEmpty then
-      //val size = (columns.length + 7) / 8
-      //
-      // @annotation.tailrec
-      // def buildBitVector(index: Int, acc: BitVector): BitVector =
-      //  if index == size then acc
-      //  else buildBitVector(index + 1, acc |+| BitVector(0))
-
-      //buildBitVector(0, BitVector.empty)
       if columns.contains(ColumnDataType.MYSQL_TYPE_NULL) then
         val bitmap = columns.reverse.foldLeft(0) { (bitmap, param) =>
           param match
             case ColumnDataType.MYSQL_TYPE_NULL => (bitmap << 1) | 1
             case _                              => (bitmap << 1) | 0
         }
-        println(s"bitmap: $bitmap")
         uint8.encode(bitmap).require
       else BitVector(0)
     else BitVector.empty
